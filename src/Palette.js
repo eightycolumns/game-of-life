950491899f8669e217e9a9e3c9a25f604364faac
Drew Keller <drew@eightycolumns.com>
2017-10-15 21:45:12 -0400

    Generate random limits

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,18 @@
 class Palette {}
 
+function randomLimits() {
+  const randomIntegerOne = randomInteger(0, 255);
+  const randomIntegerTwo = randomInteger(0, 255);
+
+  return {
+    lower: Math.min(randomIntegerOne, randomIntegerTwo),
+    upper: Math.max(randomIntegerOne, randomIntegerTwo)
+  };
+}
+
 function randomInteger(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
 export default Palette;
-export {randomInteger};
+export {randomLimits, randomInteger};
--- src/Palette.test.js
+++ src/Palette.test.js
@@ -1,4 +1,18 @@
-import {randomInteger} from "./Palette";
+import {randomLimits, randomInteger} from "./Palette";
+
+describe("a random lower limit", () => {
+  it("is less than or equal to its corresponding upper limit", () => {
+    const limits = randomLimits();
+    expect(limits.lower).toBeLessThanOrEqual(limits.upper);
+  });
+});
+
+describe("a random upper limit", () => {
+  it("is greater than or equal to its corresponding lower limit", () => {
+    const limits = randomLimits();
+    expect(limits.upper).toBeGreaterThanOrEqual(limits.lower);
+  });
+});
 
 describe("a random integer", () => {
   it("is greater than or equal to the min value", () => {
