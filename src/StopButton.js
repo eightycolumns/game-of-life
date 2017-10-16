eb04d2f8630c7d4750cbc3c97884703f96c41cb5
Drew Keller <drew@eightycolumns.com>
2017-10-15 20:00:42 -0400

    Convert StopButton to functional component

--- src/StopButton.js
+++ src/StopButton.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-class StopButton extends React.Component {
-  render() {
-    return (
-      <div className="StopButton">
-        <button onClick={this.props.onClick}>Stop</button>
-      </div>
-    );
-  }
+function StopButton(props) {
+  return (
+    <div className="StopButton">
+      <button onClick={props.onClick}>Stop</button>
+    </div>
+  );
 }
 
 export default StopButton;
