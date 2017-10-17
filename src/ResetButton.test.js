ebb73b7f992f5ee538fe2c863bcf718e14ad39cb
Drew Keller <drew@eightycolumns.com>
2017-10-17 12:53:57 -0400

    Test that ResetButton renders without crashing

--- src/ResetButton.test.js
+++ src/ResetButton.test.js
@@ -4,6 +4,12 @@ import React from "react";
 import ResetButton from "./ResetButton";
 
 describe("a ResetButton component", () => {
+  it("renders without crashing", () => {
+    shallow(
+      <ResetButton onClick={function resetGame() {}} />
+    );
+  });
+
   it("renders correctly", () => {
     const resetButton = shallow(
       <ResetButton onClick={function resetGame() {}} />
