6c8c84d4deb12b91d22589116a8986e3f359f675
Drew Keller <drew@eightycolumns.com>
2017-10-17 09:08:03 -0400

    Test that App renders without crashing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import {render} from "enzyme";
+import {render, shallow} from "enzyme";
 import React from "react";
 
 import App from "./App";
 
 describe("an App component", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
   it("renders correctly", () => {
     const app = render(<App />);
     expect(app).toMatchSnapshot();
