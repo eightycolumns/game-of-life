0b3e5d207c3ae5c3dd097453f3af1ec918efb999
Drew Keller <drew@eightycolumns.com>
2017-10-17 12:45:35 -0400

    Test that Grid renders without crashing

--- src/Grid.test.js
+++ src/Grid.test.js
@@ -6,6 +6,21 @@ import Grid from "./Grid";
 import Palette from "./Palette";
 
 describe("a Grid component", () => {
+  it("renders without crashing", () => {
+    const nRows = 3;
+    const nCols = 3;
+
+    render(
+      <Grid
+        nRows={nRows}
+        nCols={nCols}
+        prevGrid={undefined}
+        grid={Game.initialGrid(nRows, nCols)}
+        palette={new Palette()}
+      />
+    );
+  });
+
   it("renders correctly", () => {
     const nRows = 3;
     const nCols = 3;
