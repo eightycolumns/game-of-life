9128bceef9bb9ec4f238bd544d6594233a45c08b
Drew Keller <drew@eightycolumns.com>
2017-10-17 12:51:52 -0400

    Test that StopButton renders without crashing

--- src/StopButton.test.js
+++ src/StopButton.test.js
@@ -4,6 +4,12 @@ import React from "react";
 import StopButton from "./StopButton";
 
 describe("a StopButton component", () => {
+  it("renders without crashing", () => {
+    shallow(
+      <StopButton onClick={function stopGame() {}} />
+    );
+  });
+
   it("renders correctly", () => {
     const stopButton = shallow(
       <StopButton onClick={function stopGame() {}} />
