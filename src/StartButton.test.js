c9bb791b39b4f6bca758a6f57d9e584edaebfb6d
Drew Keller <drew@eightycolumns.com>
2017-10-17 12:50:25 -0400

    Test that StartButton renders without crashing

--- src/StartButton.test.js
+++ src/StartButton.test.js
@@ -4,6 +4,12 @@ import React from "react";
 import StartButton from "./StartButton";
 
 describe("a StartButton component", () => {
+  it("renders without crashing", () => {
+    shallow(
+      <StartButton onClick={function startGame() {}} />
+    );
+  });
+
   it("renders correctly", () => {
     const startButton = shallow(
       <StartButton onClick={function startGame() {}} />
